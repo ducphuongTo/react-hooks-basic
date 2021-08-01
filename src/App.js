b08c9ecbad95ac69a2a428ae39dcf916954c1560
toducphuong7900@gmail.com <toducphuong7900@gmail.com>
2021-08-01 19:50:49 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss'
+import React from 'react'
+import ColorBox from './components/ColorBox'
+const App = () => {
+  return (
+    <div className='app'>
+        <h1>Welcome to react Hooks!</h1>
+        <ColorBox/>
+    </div>
+  )
+}
+
+export default App
+
+
